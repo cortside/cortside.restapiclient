d64421f690878fa29a7a9c8a563bf9b70a71b587
Cort Schaefer <cort@eschaefer.net>
2023-10-18 12:53:43 -0600

    Add extension method for easier logging of response failures

---
+++ src/Cortside.RestApiClient/MessageExceptions/ExternalCommunicationFailureMessage.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Runtime.Serialization;
+using Cortside.Common.Messages.MessageExceptions;
+
+namespace Cortside.RestApiClient.MessageExceptions {
+    [Serializable]
+    public class ExternalCommunicationFailureMessage : InternalServerErrorResponseException {
+        public ExternalCommunicationFailureMessage(string message) : base(message) {
+        }
+
+        public ExternalCommunicationFailureMessage() : base("error communicating with an external service.") {
+        }
+
+        protected ExternalCommunicationFailureMessage(SerializationInfo info, StreamingContext context) : base(info, context) {
+        }
+
+        public ExternalCommunicationFailureMessage(string message, Exception exception) : base(message, exception) {
+        }
+    }
+}
--- src/Cortside.RestApiClient/RestResponseExtensions.cs
+++ src/Cortside.RestApiClient/RestResponseExtensions.cs
@@ -1,4 +1,18 @@
+using System;
+using Cortside.RestApiClient.MessageExceptions;
+using Microsoft.Extensions.Logging;
+using RestSharp;
+
 namespace Cortside.RestApiClient {
-    public class RestResponseExtensions {
+    public static class RestResponseExtensions {
+        public static Exception LoggedFailureException(this RestResponse response, ILogger logger, string message, params object[] args) {
+            if (response.ErrorException != null) {
+                logger.LogError(response.ErrorException, message, args);
+                return new ExternalCommunicationFailureMessage(string.Format(message, args), response.ErrorException);
+            }
+
+            logger.LogError(message, args);
+            return new ExternalCommunicationFailureMessage(string.Format(message, args));
+        }
     }
 }
